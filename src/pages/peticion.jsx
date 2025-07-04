2cef018ba717715c854202d7d5d9b16c080e72f6
Web <sotojohanestiven@gmail.com>
2025-07-04 01:41:37 -0500

    cambios peticion

---
+++ src/pages/peticion.jsx
@@ -0,0 +1,12 @@
+import './peticion.css';
+
+function Peticion() {
+  return (
+    <div>
+      <h2>Formulario de Petición</h2>
+      {/* Aquí va el formulario */}
+    </div>
+  );
+}
+
+export default Peticion;
